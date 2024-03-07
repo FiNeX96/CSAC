25fc8fafa4734cb835002b0550d30a362ed51868
FiNeX96 <rodrigoaguiar96@ua.pt>
2024-03-07 09:18:42 +0000

    update C version from stor

--- Proj_1/Python/count_sum_free_sets_v1.py
+++ Proj_1/Python/count_sum_free_sets_v1.py
@@ -1,38 +1,43 @@
-from elapsed_time import cpu_time
+import time
 
-max_n = 40
-a = []
-sum_count = []
-count = []
+max_n = 60
 
 def recursive(first_to_try, set_size):
     
-    while (first_to_try; first_to_try < max_n; first_to_try += 1):
+    while (first_to_try < temp_max):
         
         if(sum_count[first_to_try] == 0):
             count[first_to_try] += 1
             
             a[set_size] = first_to_try
             
-            for (i = 0; i <= set_size; i += 1):
+            for i in range(set_size+1):
                 sum_count[a[i] + first_to_try] += 1
             
             recursive(first_to_try + 1, set_size +1)
             
-            for (i = 0; i <= set_size; i += 1):
+            for i in range(set_size+1):
                 sum_count[a[i] + first_to_try] -= 1
+            
+        first_to_try+=1
                 
 
-if __name__ == "__man__":
-    count[0] = 1
+if __name__ == "__main__":
     
-    recursive(1, 0)
     
-    for (i = 0; i <= max_n; i += 1):
-        
-        if (i > 0):
-            count[i] += count[i - 1]
+    for i in range(1, max_n + 1):
+        temp_max = i
+        a = []
+        sum_count = []
+        count = []
+        count.append(1)
+        temp_max = 0
+        start_time = time.time()
+        recursive(1,0)
+        end_time = time.time()
+        for j in range(0, temp_max):
+            if (j > 0):
+                count[j] += count[j - 1]
+        print(len(count))
+        print(i,count[-1]+1 , end_time - start_time)
     
-        print(f'{i}  -  {count[i]}')
-    
-    print(f'# {max_m} {cpu_time()}')
