8fd760ee0b216af09e26c5577ac54b9c84289860
WildBunnie <wildbunnie2003@gmail.com>
2024-06-23 13:17:17 +0100

    added crystal script and exe times

---
+++ Proj_1/Crystal/measure.bash
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+# Check if cpu_name is provided
+if [ -z "$1" ]; then
+    echo "Usage: $0 \"<cpu_name>\" [num_iterations]"
+    exit 1
+fi
+
+# Get the number of iterations and identifier from the user, set defaults if not provided
+cpu_name=$1
+num_iterations=${2:-50}
+
+# Make it lower case, squeeze the spaces and turn them into underscores
+cpu_name=$(echo "$cpu_name" | tr '[:upper:]' '[:lower:]' | tr -s ' ' | tr ' ' '_')
+
+# Compile align_columns to be used later
+cc -Wall -O2 -Wextra -Wundef ../align_columns.c -o align_columns
+
+crystal count_sum_free_sets_v1_plain.cr -n ${num_iterations} > "../exec_times/Crystal/${cpu_name}_v1_crystal.txt"
+
+./process_execution_time_file.bash "../exec_times/Crystal/${cpu_name}_v1_crystal.txt"
